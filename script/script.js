eb2f0e14057d5d1c2d4bc8721659a5f93bb49584
FrancescoSisti <francescosisti61@gmail.com>
2024-09-05 20:17:13 +0200

    last commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
 console.log(`JS OK`);
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            messaggio: 'Benvenuto nel mio sito!',
+            immagineUrl: 'https://picsum.photos/400/300'
+        }
+    }
+}).mount('#app');
